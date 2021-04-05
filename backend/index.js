db741db85f82682b26fea0a7a229bae53cfcf0c0
kevhunte <kevinmhunte@gmail.com>
2021-04-05 08:08:18 -0400

    backend

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+// root of server
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+})
