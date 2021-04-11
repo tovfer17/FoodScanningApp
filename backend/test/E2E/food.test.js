8ac8d3fb03b443495640fdf9a45b3cc015f7453d
kevhunte <kevinmhunte@gmail.com>
2021-04-11 11:20:29 -0400

    Dummy Tests + Node-fetch lib

---
+++ backend/test/E2E/food.test.js
@@ -0,0 +1,10 @@
+const fetch = require('node-fetch')
+const assert = require('assert')
+
+const url = 'http://localhost:3000/user'
+
+describe('Dummy Food Test',() => {
+    it('Passes',() => {
+        assert.strictEqual(true,true)
+    })
+})
---
+++ backend/test/E2E/user.test.js
@@ -0,0 +1,10 @@
+const fetch = require('node-fetch')
+const assert = require('assert')
+
+const url = 'http://localhost:3000/food'
+
+describe('Dummy User Test',() => {
+    it('Passes',() => {
+        assert.strictEqual(true,true)
+    })
+})
