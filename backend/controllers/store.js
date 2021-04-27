50af81a8a88dd977e740578c34b043025c86912f
kevhunte <kevinmhunte@gmail.com>
2021-04-27 17:09:50 -0400

    Google API implementation

--- backend/controllers/store.js
+++ backend/controllers/store.js
@@ -8,29 +8,29 @@ store.get('/', (req, res) => {
     res.send('Hello world from store controller')
 })
 
-store.get('/:storeId/', (req,res) => {
-    let storeId = req.params.storeId
+// store.get('/:storeId/', (req,res) => {
+//     let storeId = req.params.storeId
 
-    if (storeId) {
-        getStoreFromGoogleById(storeId).then((result) => {
-            console.log('store info - ' ,result);
-            if (result){
-                res.status(200).json(result)
-            }
-            else{
-                res.status(404).json({
-                    ErrorMessage: "Store not found"
-                })
-            }
+//     if (storeId) {
+//         getStoreFromGoogleById(storeId).then((result) => {
+//             console.log('store info - ' ,result);
+//             if (result){
+//                 res.status(200).json(result)
+//             }
+//             else{
+//                 res.status(404).json({
+//                     ErrorMessage: "Store not found"
+//                 })
+//             }
             
-        })
-    }
-    else {
-        res.status(400).json({
-            ErrorMessage: "storeId is required"
-        })
-    }
-})
+//         })
+//     }
+//     else {
+//         res.status(400).json({
+//             ErrorMessage: "storeId is required"
+//         })
+//     }
+// })
 
 store.get('/items/:foodId/',(req, res) => {
     let foodId = req.params.foodId
@@ -44,7 +44,6 @@ store.get('/items/:foodId/',(req, res) => {
         if (foodResult) {
 
             listStoresFromGoogle(foodResult.name, long, lat).then((storesResult) => {
-                console.log('stores list - ', storesResult);
                 res.status(200).json(storesResult)
             })
         }
--- backend/relay/store.js
+++ backend/relay/store.js
@@ -1,11 +1,34 @@
 const fetch = require('node-fetch')
 const store = require('../controllers/store')
 
-//https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJrTLr-GyuEmsRBfy61i59si0&key=YOUR_API_KEY
-const getStoreFromGoogleById = async (storeId) => {}
+const cache = []
 
-//https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=mongolian%20grill&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&locationbias=circle:2000@47.6918452,-122.2226413&key=YOUR_API_KEY
-const listStoresFromGoogle = async (name,long, lat) => {}
+const getStoreFromGoogleById = async (storeId) => {
+    let res = await fetch(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${storeId}&key=${process.env.GOOGLE_API_KEY}`)
+
+    let data = await res.json()
+
+    if (data && res.status === 200){
+        return data
+    }
+    else {
+        return null
+    }
+}
+
+const listStoresFromGoogle = async (name, long, lat, radius=2000) => {
+    let encoded_name = encodeURI(name)
+    let res = await fetch(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${encoded_name}&inputtype=textquery&fields=photos,formatted_address,name,opening_hours&locationbias=circle:${radius}@${lat},${long}&key=${process.env.GOOGLE_API_KEY}`)
+
+    let data = await res.json()
+
+    if (data && res.status === 200){
+        return data.candidates
+    }
+    else {
+        return null
+    }
+}
 
 
 module.exports = {getStoreFromGoogleById, listStoresFromGoogle}
